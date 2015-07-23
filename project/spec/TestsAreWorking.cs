5191a0bceb002af169f4a0a0ab604488dae3ba1f
Arlo Belshee <arlo.belshee@live.com>
2015-07-23 10:42:21 -0700

    Empty project that runs one test. Time to start making some awful!

---
+++ project/spec/TestsAreWorking.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace project.spec
+{
+	[TestFixture]
+	public class TestsAreWorking
+	{
+
+		[Test]
+		public void TestShouldRun()
+		{
+			1.Should().Be(1);
+		}
+	}
+}
