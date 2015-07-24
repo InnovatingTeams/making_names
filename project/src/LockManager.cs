e492b5c113d4be286ba9b56cdff521f572d79e3f
Arlo Belshee <arlo.belshee@live.com>
2015-07-23 17:16:48 -0700

    Sketch out the function that I'll have people work with.

---
+++ project/src/LockManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace project
+{
+	public class LockManager
+	{
+		private class Lock : IDisposable
+		{
+			private readonly Mutex _mutex = new Mutex();
+
+			public void Dispose()
+			{
+				_mutex.ReleaseMutex();
+				_mutex.Dispose();
+			}
+		}
+
+		private static readonly ConcurrentDictionary<string, Lock> Locks = new ConcurrentDictionary<string, Lock>();
+
+		public static IDisposable GetLock(string @lock)
+		{
+			return Locks.GetOrAdd(@lock, _=>new Lock());
+		}
+	}
+}
---
+++ project/src/TailManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+
+namespace project
+{
+	public class TailManager
+	{
+		public void OnUpdate(string code)
+		{
+			Task<string> finished;
+			using (LockManager.GetLock("Network"))
+			{
+				// Prepare to poll service
+
+				// Poll the service
+			}
+
+			finished.ContinueWith(s =>
+			{
+				// Parse the response
+				var response = JsonConvert.ParseObject(s.Result);
+
+				var tails = response.GetObject(code).GetArray("Tails");
+
+				using (LockManager.GetLock("database"))
+				{
+					// query for all the tails in the database
+
+					// find the two deltas between the sets
+
+					// Write new tails to the database
+
+					// Remove missing tails from the database
+
+					// Same upates to the cache
+
+				}
+
+				// Kick off background processing on new tails
+
+				// Notify the UI to redraw itself
+			});
+		}
+	}
+}
