b4d6310a58e22a1e112b08544b2cdfd25ded16e5
arlobelshee <arlo@belshee.com>
2020-08-31 16:40:28 -0400

    r   Rename 4 methods to [narrow] the unknown.

--- project/src/ChartDisplay.cs
+++ project/src/ChartDisplay.cs
@@ -15,13 +15,13 @@ namespace project
             ChartInfo info,
             ChartData data)
         {
-            probably_DrawBackgroundAndLayOutAreas_AndStuffButNotText(ctx, canvas, info);
+            probably_DrawBackgroundAndLayOutAreas_AndDirectlyDrawOtherChartInfoButNotText(ctx, canvas, info);
             probably_AskProvidersForDataContextInfo_AndStuff(ctx, canvas, data, providers);
-            probably_DrawAxes_AndStuff(ctx, canvas, data);
+            probably_DrawAxes_AndDirectlyDrawOtherData(ctx, canvas, data);
             markers.ForEach(m =>
                 m.probably_DrawMarks_AndStuff(ctx, canvas, data, providers, info, data));
-            probably_AddTextForTitlesAndAxisLabels_AndStuff(ctx, canvas, info);
-            probably_AddTextForDataLabels_AndStuff(ctx, canvas, data);
+            probably_AddTextForTitlesAndAxisLabels_AndDirectlyDrawOtherChartInfo(ctx, canvas, info);
+            probably_AddTextForDataLabels_AndDirectlyDrawOtherData(ctx, canvas, data);
         }
 
         private static void probably_AskProvidersForDataContextInfo_AndStuff(
@@ -35,22 +35,22 @@ namespace project
             providers.ForEach(p => p.DrawNotes(ctx, canvas, data.AllMetadata));
         }
 
-        private static void probably_AddTextForDataLabels_AndStuff(DC ctx, Canvas canvas, ChartData data)
+        private static void probably_AddTextForDataLabels_AndDirectlyDrawOtherData(DC ctx, Canvas canvas, ChartData data)
         {
             throw new System.NotImplementedException();
         }
 
-        private static void probably_AddTextForTitlesAndAxisLabels_AndStuff(DC ctx, Canvas canvas, ChartInfo info)
+        private static void probably_AddTextForTitlesAndAxisLabels_AndDirectlyDrawOtherChartInfo(DC ctx, Canvas canvas, ChartInfo info)
         {
             throw new System.NotImplementedException();
         }
 
-        private static void probably_DrawAxes_AndStuff(DC ctx, Canvas canvas, ChartData data)
+        private static void probably_DrawAxes_AndDirectlyDrawOtherData(DC ctx, Canvas canvas, ChartData data)
         {
             throw new System.NotImplementedException();
         }
 
-        private static void probably_DrawBackgroundAndLayOutAreas_AndStuffButNotText(DC ctx, Canvas canvas,
+        private static void probably_DrawBackgroundAndLayOutAreas_AndDirectlyDrawOtherChartInfoButNotText(DC ctx, Canvas canvas,
             ChartInfo info)
         {
             throw new System.NotImplementedException();
